21758d3a700785770185d1c453701cbf69ff825d
Nathan Pickard <nathan.pickard87@gmail.com>
2018-03-07 22:40:17 -0800

    Child routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,21 @@ import { LoginComponent } from "./login/login.component";
 import { ResetComponent } from "./reset-password/reset-password.component";
 import { OutletComponent } from "./outlet/outlet.component";
 import { NotFoundComponent } from "./404/404.component";
-import { ProductServices } from './services/product.service';
-import { DetailsComponent } from './details/details.component';
+import { ProductServices } from './Products/services/product.service';
+import { DetailsComponent } from './Products/details/details.component';
 import { InstructionsComponent } from './instructions/instructions.component';
+import { ItemListComponent } from './Products/item-list/item-list.component';
+import { ItemDetailsComponent } from './Products/item-details/item-details.component';
+import { ItemEditComponent } from './Products/edit/item-edit.component';
+import { UsersComponent } from './admin/users/users.component';
+import { AddUserComponent } from './admin/add-user/add-user.component';
+import { EditUserComponent } from './admin/edit-user/edit-user.component';
+import { PermissionsComponent } from './admin/permissions/permissions.component';
+import { LoginUserComponent } from './login/login-user/login-user.component';
+import { ResetPasswordComponent } from './login/reset-password/reset-password.component';
+import { ForgotPasswordComponent } from './login/forgot-password/forgot-password.component';
+import { AdminComponent } from './admin/admin.component';
+import { ProductsComponent } from './Products/products.component';
 
 const appRoutes: Routes = [
   { path: '', component: InstructionsComponent },
